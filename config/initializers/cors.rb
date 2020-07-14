bc7cbcea3c04712a8bec8203358d5b1cec9426d6
thuyanduong <thuyanannduong@gmail.com>
2020-07-14 17:34:06 -0400

    update endpoint

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://animal-crossing-memory-match.herokuapp.com'
+    origins 'https://animal-crossing-memory-match.herokuapp.com/'
 
     resource '*',
       headers: :any,
